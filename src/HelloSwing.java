25211c5139e3cf353672fc86616ee5e6f5971cdf
joao paulo <37601838+joaopaulopossamai@users.noreply.github.com>
2023-05-17 22:35:59 -0300

    test

---
+++ src/HelloSwing.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HelloSwing extends JFrame{
+    private JButton buttonMensagem;
+    public JPanel panelPrincipal;
+    private JTextField textMensagem;
+
+    public HelloSwing() {
+    buttonMensagem.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(buttonMensagem,
+                    textMensagem.getText());
+        }
+    });
+}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mudei no github");
+
+        HelloSwing form = new HelloSwing();
+        form.setContentPane(form.panelPrincipal);
+        form.setTitle("Hello Panel");
+        form.setSize(500,500);
+        form.setVisible(true);
     }
 }
